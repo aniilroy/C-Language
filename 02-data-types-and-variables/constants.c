ca398638b6bf34c347eee250b70e870929485217
Anil Roy <anilroy.cce@gmail.com>
2026-06-12 00:28:23 +0530

    add constants.c

---
+++ 02-data-types-and-variables/constants.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+// Program demonstrating constants in C
+
+const float PI=3.14159f;
+
+const int DAYS_IN_WEEK=7;
+
+const char grade='A';
+
+printf("FI= %.4f\n",PI );
+
+printf("Days in a Week: %d\n", DAYS_IN_WEEK);
+
+printf("Grade: %c", grade);
+
+return 0;
+
+}
