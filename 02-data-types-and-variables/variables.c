62440d9ba0b06d5059251fdebf2ab6635d43ebde
Anil Roy <anilroy.cce@gmail.com>
2026-06-12 00:16:58 +0530

    add variables.c

---
+++ 02-data-types-and-variables/variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    //Program demonstrating variables in C
+    int age=30;
+
+    char grade='A';
+
+    float height=5.8;
+
+    printf("The age is : %d\n",age);
+
+    printf("The grade is: %c\n", grade);
+
+    printf("The height is : %.2f\n", height);
+
+    return 0;
+}
