d15fef01d8abaeace34aea7051ae729c4babe994
Anil Roy <anilroy.cce@gmail.com>
2026-06-12 14:06:19 +0530

    add relational.c operator program

---
+++ 03-operators/relational.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+ int a = 10 , b = 20;
+
+ printf("a = %d\n", a);
+
+ printf("b = %d\n", b);
+
+ printf("a == b : %d\n", a==b);
+
+ printf("a != b : %d\n", a!=b);
+
+ printf("a > b : %d\n", a>b);
+
+ printf("a < b : %d\n", a<b);
+
+ printf("a >= b : %d\n", a>=b);
+
+ printf("a <= b : %d\n", a<=b);
+
+ return 0;
+ 
+}
