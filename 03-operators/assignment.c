c56fcb4579a347436bd68c046fe18b2bf654aa44
Anil Roy <anilroy.cce@gmail.com>
2026-06-12 14:53:05 +0530

    add assignment.c assignment operators program

---
+++ 03-operators/assignment.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+    int x = 10;
+
+    printf("Initial value of x: %d\n", x);
+
+    x += 5; // x = x + 5
+
+    printf("After x += 5 : %d\n", x);
+
+    x -= 3; // x = x - 3
+
+    printf("After x -= 3 : %d\n", x);
+
+    x *= 2; // x = x * 4
+
+    printf("After x *= 2 : %d\n", x);
+
+    x /= 4; // x = x / 4
+
+    printf("After x /= 4 : %d\n", x);
+
+    x %= 3; // x = x % 3
+
+    printf("After x %%= 3 : %d\n", x);
+
+    return 0;
+}
