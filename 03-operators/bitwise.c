577ce7465689fad458c17c3d49be369de20ad910
Anil Roy <anilroy.cce@gmail.com>
2026-06-14 23:41:34 +0530

    add bitwise.c bitwise operators

---
+++ 03-operators/bitwise.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=12;
+
+    int b=5;
+
+    //Bitwise AND
+
+    printf("a & b : %d\n", a & b);
+
+    //Bitwise OR
+
+    printf("a | b : %d\n", a | b);
+
+    //Bitwise XOR 
+
+    printf("a ^ b : %d\n", a ^ b);
+
+    //Bitwise NOT
+
+    printf("~a : %d\n", ~a);
+
+    //Left shift
+
+    printf("a << 2: %d\n", a << 2);
+
+    //Bitwise Right shift
+
+    printf("a >> 2 : %d\n", a >> 2);
+
+    return 0;
+
+}
