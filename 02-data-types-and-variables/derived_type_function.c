a38ff5659dcaf36d24d45c4487ee50563e37285a
Anil Roy <anilroy.cce@gmail.com>
2026-06-15 00:45:33 +0530

    Add derived_type_function.c

---
+++ 02-data-types-and-variables/derived_type_function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+    int add( int x, int y){
+
+        return x + y;
+    }
+
+    int main(){
+
+        int result;
+
+        result= add(15 , 45);
+
+        printf("Sum of Two Number: %d", result);
+
+        return 0;
+
+    }
