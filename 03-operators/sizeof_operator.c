f346bfa1a44511e4b1940c523d0e3e9f58f3d482
Anil Roy <anilroy.cce@gmail.com>
2026-06-14 23:56:35 +0530

    Add sizeof_operator.c sizeof operator

---
+++ 03-operators/sizeof_operator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+
+    float b;
+
+    double c;
+    char d;
+
+    printf("Size of int : %zu bytes\n", sizeof(a));
+
+    printf("Size of float : %zu bytes\n", sizeof(b));
+
+    printf("Size of double : %zu bytes\n", sizeof(c));
+
+    printf(" Size of Char : %zu bytes\n", sizeof(d));
+
+    return 0;
+
+
+}
